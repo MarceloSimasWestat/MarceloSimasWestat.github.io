e74a388b2aa307e34c99284d5d6250d77ff1624f
fucci_a <anthonyfucci@westat.com>
2016-10-28 12:55:01 -0400

    adding preschool data create-table-topojson objects

--- Discipline/spatial/export_schooldistricts.cmd
+++ Discipline/spatial/export_schooldistricts.cmd
@@ -1,4 +1,5 @@
 python postgis2geojson-master/postgis2geojson.py -d absenteeism -H atl-pgsqltest.westat.com -P 5432 -u postgres -p postgres1 -t schooldistricts_medium_with_features_g5420_g5400 -g geom -o medium_G5420_G5400 --topojson
 python postgis2geojson-master/postgis2geojson.py -d absenteeism -H atl-pgsqltest.westat.com -P 5432 -u postgres -p postgres1 -t schooldistricts_medium_with_features_g5410 -g geom -o medium_G5410 --topojson
-python postgis2geojson-master/postgis2geojson.py -d absenteeism -H atl-pgsqltest.westat.com -P 5432 -u postgres -p postgres1 -t schooldistricts_medium_with_features_not_simplified -g geom -o medium_not_simplified --topojson
+python postgis2geojson-master/postgis2geojson.py -d absenteeism -H atl-pgsqltest.westat.com -P 5432 -u postgres -p postgres1 -t schooldistricts_medium_with_features_not_simplified -g geom -o extra_districts --topojson
+python postgis2geojson-master/postgis2geojson.py -d absenteeism -H atl-pgsqltest.westat.com -P 5432 -u postgres -p postgres1 -t schooldistricts_medium_state_borders -g geom -o states --topojson
 PAUSE
